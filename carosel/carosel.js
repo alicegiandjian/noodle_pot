7c5f69c30d85fbc41f6f36e55cae475c73dd75fc
kdubtesting <kcw11kx@gmail.com>
2020-06-01 02:23:57 -0700

    quick fix

--- carosel/carosel.js
+++ carosel/carosel.js
@@ -1,8 +1,6 @@
 var carosel = document.getElementById("carosel-container");
 var cW = parseInt($(carosel).css("width"));
 var cH = parseInt($(carosel).css("height"));
-var ogW = cW;
-var ogH = cH;
 
 var moveCont = document.getElementsByClassName("move-contain");
 var imgCont = document.getElementsByClassName("img-container");
@@ -63,37 +61,6 @@ for(var i = 0; i < imgCont.length; i++)
 }
 
 var roll = null;
-
-/*ar roll = setTimeout(function scroll(){
-
-	for(var i = 0; i < moveCont.length; i++)
-	{
-		var l = parseInt($(moveCont[i]).css("left")) - cW;
-		$(moveCont[i]).css("left", l + "px");
-
-		if(l == 0)
-		{
-			$(moveCont[i]).css("z-index", 1000000);
-			pos[i] = 1;
-		}
-
-		if(l < 0)
-		{
-			$(moveCont[i]).css("z-index", 100);
-			pos[i] = 2;
-		}
-
-		if(l == (-2 * cW))
-		{
-			$(moveCont[i]).css("z-index", 1);
-			$(moveCont[i]).css("left", cW + "px");
-			pos[i] = 0;
-		}
-	}
-
-	roll = setTimeout(scroll, 2000);
-
-}, 2000);*/
 
 var roll = setTimeout(function scroll(){
 
@@ -247,126 +214,3 @@ function resizeend()
 	}
 }
 
-
-/*$(window).resize(function(){
-
-	var cW = parseInt($(carosel).css("width"));
-	var cH = parseInt($(carosel).css("height"));
-
-	var smallest = 0;
-
-	for(var i = 0; i < carosel.childNodes.length; i++)
-	{
-		$(carosel.childNodes[i]).css("width", cW + "px");
-
-		var nH = (sizes[i][1] * cW) / sizes[i][0];
-
-		if(i == 0)
-		{
-			smallest = nH;
-		}
-		else
-		{
-			if(nH < smallest)
-			{
-				smallest = nH;
-			}
-		}
-
-		if(cH >= window.innenHeight && nH >= cH)
-		{
-			$(carosel).css("height", window.innenHeight + "px");
-			$(imgCont[i]).css("height", window.innenHeight + "px");
-			$(carosel.childNodes[i]).css("height", nH + "px");
-			smallest = window.innenHeight;
-		}
-		else
-		{
-			$(carosel).css("height", smallest + "px");
-			$(imgCont[i]).css("height", smallest + "px");
-			$(img[i]).css("height", nH + "px");
-		}
-
-	}
-
-	carosel.style.height = smallest + "px";
-
-	for(var i = 0; i < imgCont.length; i++)
-	{
-		imgCont[i].style.height = smallest + "px";
-		var h = parseInt($(imgs[i]).css("height"));
-		var t = (smallest / 2) - (h / 2);
-		imgs[i].style.top = t + "px";
-	}
-
-});*/
-
-/*var carosel = document.getElementById("carosel-container");
-var cW = parseInt($(carosel).css("width"));
-var cH = parseInt($(carosel).css("height"));
-
-var sizes = [];
-
-console.log(carosel.childNodes);
-
-var c = 0;
-
-for(var i = 0; i <= carosel.childNodes.length; i+=2)
-{
-	carosel.removeChild(carosel.childNodes[c]);
-	c++;
-}
-
-console.log(carosel.childNodes);
-
-for(var i = 0; i < carosel.childNodes.length; i++)
-{
-	var ogW = parseInt($(carosel.childNodes[i]).css("width"));
-	var ogH = parseInt($(carosel.childNodes[i]).css("height"));
-	sizes.push(new Array());
-	sizes[i].push(ogW);
-	sizes[i].push(ogH);
-	$(carosel.childNodes[i]).css("width", cW + "px");
-
-	var nH = (ogH * cW) / ogW;
-
-	console.log(nH);
-	console.log(cH);
-
-	if(nH < cH)
-	{
-		console.log("less than");
-		$(carosel.childNodes[i]).css("height", nH + "px");
-		$(carosel).css("height", nH + "px");
-	}
-	else
-	{
-		$(carosel.childNodes[i]).css("height", nH + "px");
-	}
-}
-
-$(window).resize(function(){
-
-	var cW = parseInt($(carosel).css("width"));
-	var cH = parseInt($(carosel).css("height"));
-
-	for(var i = 0; i < carosel.childNodes.length; i++)
-	{
-		$(carosel.childNodes[i]).css("width", cW + "px");
-
-		var nH = (sizes[i][1] * cW) / sizes[i][0];
-
-		if(cH >= window.innenHeight && nH >= cH)
-		{
-			$(carosel).css("height", window.innenHeight + "px");
-			$(carosel.childNodes[i]).css("height", nH + "px");
-		}
-		else
-		{
-			$(carosel.childNodes[i]).css("height", nH + "px");
-			$(carosel).css("height", nH + "px");
-		}
-
-	}
-
-});*/
